cd0dd7dfb5d5a9ca10aee1096ca84529b0942dc4
KathleenQueen <3160103013@zju.edu.cn>
2019-11-21 12:57:44 +0800

    add

--- studying/dataStructuresAlgorithmswithJS/test_4_1.js
+++ studying/dataStructuresAlgorithmswithJS/test_4_1.js
@@ -1,7 +1,9 @@
+// 栈可以用来判断一个算术表达式中的括号是否匹配。
+// 编写一个函数，该函数接受一个算术表达式作为参数，返回括号不匹配的位置。
+// 下面是一个括号不匹配的算术表达式的例子：2.3 + 23 / 12 + (3.14159 * 0.24。
 function Stack() {
     this.dataStore=[];
     this.top=0;
-
     this.push=push;
     this.pop=pop;
     this.peek=peek;
@@ -23,21 +25,22 @@ function length() {
 function clear() {
     this.top=0;
 }
-var ArithmeticExpression="2.3+23/12+(3.14159*0.24";
+var ArithmeticExpression = "2.3+23/12+(3.14159*0.24";
 function brackets(n) {
     var s = new Stack();
-    var countleft = 0, countright = 0;
-    var leftbrackets= ['(', '{', '['];
-    var rightbrackets = [')', '}', ']'];
-    for(var i=0;i<n.length;++i){
-        s.push(n[i]);
-        if(n[i]=='('){countleft++;}
-        else if(n[i]==')'){countright++;}
-    }
-    if(countleft==countright) {return "No brackets missing";}
-    else if(countleft>countright){
-        for (;;){}
-    }
-    console.log(s);
+   for(var i = 0;i < n.length; ++i){
+       if(n[i]==="("){
+           s.push(i);
+       }
+           else if(n[i]===")"){
+               s.pop();
+       }
+   }
+   if(s.length()!=0){return n+"括号不匹配的位置为第"+(s.peek()+1)+"个字符";}
+   else {return n+"未出现括号不匹配现象"}
 }
 console.log(brackets(ArithmeticExpression));
+ArithmeticExpression = "2.3+23/12+(3.14159*0.24)";
+console.log(brackets(ArithmeticExpression));
+
+//finished
